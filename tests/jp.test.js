14f454886654db008f2f9cba366f08527f0a71a7
Mauviel Jean-philippe <jp@lilm.co>
2020-04-01 09:47:07 +0200

    test 6 travis

--- tests/jp.test.js
+++ tests/jp.test.js
@@ -71,4 +71,10 @@ describe("Create a new user", () => {
         page = await global.__BROWSER__.newPage()
     }, timeout)
 
+    afterAll(async () => {
+        // ouvrir un onglet dans le navigateur
+        await page.goto('http://polr.web-74.com/logout');
+    }, timeout)
+
+
 });
